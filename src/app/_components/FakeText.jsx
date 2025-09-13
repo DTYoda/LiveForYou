00c3a29750558aeffa01fd9443deea59e2b0d2ff
lastlegume <lastlegume@gmail.com>
2025-09-13 11:40:18 -0400

    updated friend suspense

---
+++ src/app/_components/FakeText.jsx
@@ -0,0 +1,8 @@
+
+function FakeText() {
+    return (<div className="m-1 bg-green-400/20 rounded-xl h-3"></div>
+    );
+}
+
+
+export default FakeText;
--- src/app/friends/page.jsx
+++ src/app/friends/page.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../_components/Navbar.jsx";
 import FriendBox from "../_components/FriendBox.jsx";
 import { Suspense } from "react";
+import FakeText from "../_components/FakeText.jsx";
 const { PrismaClient } = require('../../generated/prisma')
 
 const prisma = new PrismaClient();
@@ -20,11 +21,12 @@ export default function Friends() {
 }
 
 function PlaceholderElements() {
-  return ((<div className="rounded-xl border border-black shadow-lg border-solid p-3 m-5 bg-green-500/25">
-    <h2 className="text-xl">
+  return ((<div className="rounded-xl border cursor-wait border-black shadow-lg border-solid p-3 m-5 bg-green-500/25">
+    <h2 className="text-xl justify-between flex">
       {/* <Image alt="profile picture" width="25" height="25" class="inline" src=""></Image> */}
-      <button className="text-red-500 float-right hover:text-red-700/10 transition hover:scale-130"> X </button>
+      <FakeText ></FakeText>
+      <button className="text-red-800 hover:text-gray-900 transition hover:scale-80"> X </button>
     </h2>
-    <div className="rounded-xl border-gray-500/10 border shadow-lg border-solid p-1 m-2 ">              { }</div>
+    <div className="rounded-xl border-gray-500/10 border shadow-lg border-solid p-1 m-2 ">     <FakeText></FakeText><FakeText></FakeText></div>
   </div>));
 }
