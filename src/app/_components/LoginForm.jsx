fd57dd1219c31e2161da51394599a773157ab94a
CoolGuyGames <theforestgriefer@gmail.com>
2025-09-13 09:06:52 -0400

    updated login form

--- src/app/_components/LoginForm.jsx
+++ src/app/_components/LoginForm.jsx
@@ -1,22 +1,27 @@
-import Form from 'next/form'
+import Form from "next/form";
 
 export default function LogInForm() {
-    return (
-        <div className="h-full w-full flex flex-col gap-8 rounded-2xl items-center rounded-xl border border-black shadow-lg border-solid p-3 m-5 bg-green-500/25">
+  return (
+    <div className="h-full w-full flex flex-col gap-8 items-center rounded-xl border border-black shadow-lg border-solid p-3 m-5 bg-green-500/25">
+      <Form>
+        <h2 className="text-xl">Settings</h2>
+        <div className="w-full">
+          {/* <label className="m-4">Email: <input type="email" className="w-full"></input></label><br></br> */}
+          <label className="m-4">
+            Username: <input type="text"></input>
+          </label>
+          <br></br>
 
-            <Form>
-                <h2 className="text-xl">Settings</h2>
-                <div className="w-full">
-                    {/* <label className="m-4">Email: <input type="email" className="w-full"></input></label><br></br> */}
-                    <label className="m-4">Username: <input type="text"></input></label><br></br>
-
-                    <label>Information on the home page:<select>
-                        <option value="water" defaultValue={"water"}>Cups of Water</option>
-
-                    </select></label>
-
-                </div>
-            </Form>
+          <label>
+            Information on the home page:
+            <select>
+              <option value="water" defaultValue={"water"}>
+                Cups of Water
+              </option>
+            </select>
+          </label>
         </div>
-    );
+      </Form>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from "./_components/Navbar.jsx";
+import LoginSignup from "./_components/loginsignup.jsx";
 
 export default function Home() {
   return (
     <div className="flex justify-center flex-col md:flex-row">
       <Navbar></Navbar>
-      <div className="max-w-2xl w-full border rounded-2xl"></div>
+      <div className="max-w-2xl w-full border rounded-2xl">
+        <LoginSignup></LoginSignup>
+      </div>
       <div className="w-32"></div>
     </div>
   );
