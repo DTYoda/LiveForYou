62f726744ac6a931b86e0166a5e3be8288adeb89
CoolGuyGames <theforestgriefer@gmail.com>
2025-09-13 12:23:30 -0400

    added logout

--- src/app/_components/Navbar.jsx
+++ src/app/_components/Navbar.jsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { getServerSession } from "next-auth";
+import Logout from "./logout.jsx";
 
-function Navbar() {
+async function logout() {}
+
+async function Navbar() {
+  const session = await getServerSession();
   return (
     <div className="flex justify-end  w-32 h-fill">
       <div className="flex fixed md:relative md:flex-col w-full gap-7 h-[100vh] text-2xl pt-8">
@@ -24,6 +29,7 @@ function Navbar() {
           <Link href="/settings">Settings</Link>{" "}
           <span className="block max-w-0 group-hover:max-w-full w-full transition-all duration-500 h-0.5 bg-white"></span>
         </div>
+        {!!session && <Logout></Logout>}
       </div>
     </div>
   );
--- src/app/_components/loginsignup.jsx
+++ src/app/_components/loginsignup.jsx
@@ -1,13 +1,21 @@
+import Link from "next/link";
+
 export default function LoginSignup() {
   return (
     <div className="flex flex-col w-fill h-full justify-center items-center gap-8">
       <div className="text-4xl p-16">Life For You</div>
-      <button href="/signup" className="border rounded-2xl w-32 h-12 ">
+      <Link
+        href="/signup"
+        className="border rounded-2xl w-32 h-12 flex justify-center items-center"
+      >
         SIGN UP
-      </button>
-      <button href="" className="border rounded-2xl w-32 h-12 mb-16">
+      </Link>
+      <Link
+        href="/login"
+        className="border rounded-2xl w-32 h-12 mb-16 flex justify-center items-center"
+      >
         LOG IN
-      </button>
+      </Link>
     </div>
   );
 }
