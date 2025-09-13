1b98f71f7c337cdfa3685d3816529c7dcecd3f56
lastlegume <lastlegume@gmail.com>
2025-09-13 10:45:46 -0400

    started profile db integration

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,5 +1,7 @@
 import Navbar from "../_components/Navbar.jsx";
 import ProfileMain from "../_components/ProfileMain.jsx";
+import { Suspense } from "react";
+
 const { PrismaClient } = require('../../generated/prisma')
 
 const prisma = new PrismaClient();
@@ -8,9 +10,16 @@ export default function Digest() {
     <div className="flex justify-center flex-col md:flex-row">
       <Navbar></Navbar>
       <div className="max-w-2xl w-full">
-        <ProfileMain user="Hello"></ProfileMain>
+        <Suspense fallback={<ProfileSuspense />}>
+          {/* {(prisma.users.findMany()).then((e) => { console.log(e); })} */}
+          <ProfileMain user=""></ProfileMain>
+        </Suspense>
       </div>
       <div className="w-32"></div>
     </div>
   );
+}
+
+function ProfileSuspense() {
+  return (<div className="bg-gray-400"></div>);
 }
