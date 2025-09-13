b7b8d6d8a874b690f7281ce0779dc5cd07dfba71
CoolGuyGames <theforestgriefer@gmail.com>
2025-09-13 10:26:10 -0400

    added signup

--- src/app/_components/ProfileMain.jsx
+++ src/app/_components/ProfileMain.jsx
@@ -1,12 +1,8 @@
-
 export default function ProfileMain({ user }) {
-    return (
-        <div className="h-full w-full flex flex-col gap-8 rounded-2xl items-center rounded-xl border border-black shadow-lg border-solid p-3 m-5 ">
-
-            <h2 className="text-2xl">Profile for {user}</h2>
-            <div className="w-full text-left">
-
-            </div>
-        </div>
-    );
+  return (
+    <div className="h-full w-full flex flex-col gap-8 rounded-2xl items-center border border-black shadow-lg border-solid p-3 m-5 ">
+      <h2 className="text-2xl">Profile for {user}</h2>
+      <div className="w-full text-left"></div>
+    </div>
+  );
 }
